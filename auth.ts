3c8cf274e1cf2d1ad6e669c5cc719dd84e1014eb
TheMikeBa <21992394+TheMikeBa@users.noreply.github.com>
2025-06-14 00:12:05 -0400

    Session Cart ID Cookie

--- auth.ts
+++ auth.ts
@@ -5,6 +5,8 @@ import CredentialsProvider from "next-auth/providers/credentials";
 
 import { prisma } from "@/db/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export const config = {
   pages: {
@@ -61,8 +63,6 @@ export const config = {
       session.user.name = token.name; // 👈 Add this line
       session.user.role = token.role; // 👈 Add this line
 
-      console.log("🟨 token 🟨", token);
-
       // Optionally handle session updates (like name change)
       if (trigger === "update" && token.name) {
         session.user.name = token.name;
@@ -94,6 +94,31 @@ export const config = {
       }
 
       return token;
+    },
+    authorized({ request, auth }: any) {
+      // Check for cart cookie
+      if (!request.cookies.get("sessionCartId")) {
+        // Generate cart cookie
+        const sessionCartId = crypto.randomUUID();
+
+        // Clone the request headers
+        const newRequestHeaders = new Headers(request.headers);
+
+        // Create a new response and add the new headers
+        const response = NextResponse.next({
+          request: {
+            headers: newRequestHeaders,
+          },
+        });
+
+        // Set the newly generated ses sionCartId in the response cookies
+        response.cookies.set("sessionCartId", sessionCartId);
+
+        // Return the response with the sessionCartId set
+        return response;
+      } else {
+        return true;
+      }
     },
   },
 } satisfies NextAuthConfig;
