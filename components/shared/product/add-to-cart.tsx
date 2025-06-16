91a13fd73b6d24b2db5238bb40be5992e89759dd
TheMikeBa <21992394+TheMikeBa@users.noreply.github.com>
2025-06-16 05:44:34 -0400

    9. Dynamic Cart Button

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,14 +1,20 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Plus } from "lucide-react";
 // import { useToast } from "@/hooks/use-toast";
 // import { ToastAction } from "@/components/ui/toast";
 import { toast } from "sonner";
-import { addItemToCart } from "@/lib/actions/cart.actions";
-import { CartItem } from "@/types";
+import { Cart, CartItem } from "@/types";
+import { Plus, Minus } from "lucide-react";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({
+  cart,
+  item,
+}: {
+  cart?: Cart;
+  item: Omit<CartItem, "cartId">;
+}) => {
   const router = useRouter();
   //   const { toast } = useToast();
 
@@ -52,9 +58,43 @@ const AddToCart = ({ item }: { item: CartItem }) => {
     });
   };
 
-  return (
+  // Remove item from cart
+  const handleRemoveFromCart = async () => {
+    const res = await removeItemFromCart(item.productId);
+
+    // toast({
+    //   variant: res.success ? "default" : "destructive",
+    //   description: res.message,
+    // });
+    if (res.success) {
+      toast.success(res.message || "Item removed from cart.");
+    } else {
+      toast.error(res.message || "Failed to remove item from cart.", {
+        unstyled: true,
+        classNames: {
+          toast: "bg-red-500 text-white",
+        },
+      });
+    }
+  };
+
+  const existItem =
+    cart && cart.items.find((x) => x.productId === item.productId);
+
+  return existItem ? (
+    <div>
+      <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
+        <Minus className="w-4 h-4" />
+      </Button>
+      <span className="px-2">{existItem.qty}</span>
+      <Button type="button" variant="outline" onClick={handleAddToCart}>
+        <Plus className="w-4 h-4" />
+      </Button>
+    </div>
+  ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
-      <Plus /> Add To Cart
+      <Plus className="w-4 h-4" />
+      Add to cart
     </Button>
   );
 };
