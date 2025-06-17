2201258914c91b44f91297912d093d440e18800d
TheMikeBa <21992394+TheMikeBa@users.noreply.github.com>
2025-06-17 11:21:04 -0400

    4. Payment Method Form & Update

--- app/(root)/payment-method/payment-method-form.tsx
+++ app/(root)/payment-method/payment-method-form.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import CheckoutSteps from "@/components/shared/checkout-steps";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -10,7 +9,7 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-// import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 // import { useToast } from "@/hooks/use-toast";
 import { toast } from "sonner";
 import { updateUserPaymentMethod } from "@/lib/actions/user.actions";
@@ -40,9 +39,90 @@ const PaymentMethodForm = ({
 
   const [isPending, startTransition] = useTransition();
 
+  async function onSubmit(values: z.infer<typeof paymentMethodSchema>) {
+    startTransition(async () => {
+      const res = await updateUserPaymentMethod(values);
+
+      if (!res.success) {
+        //   toast({
+        //     variant: "destructive",
+        //     description: res.message,
+        //   });
+
+        //   return;
+        toast.error(res.message || "Failed to update payment method.", {
+          unstyled: true,
+          classNames: {
+            toast: "bg-red-500 text-white",
+          },
+        });
+        return;
+      }
+
+      router.push("/place-order");
+    });
+  }
+
   return (
     <>
-      <CheckoutSteps current={2} />
+      <div className="max-w-md mx-auto">
+        <Form {...form}>
+          <form
+            method="post"
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4"
+          >
+            <h1 className="h2-bold mt-4">Payment Method</h1>
+            <p className="text-sm text-muted-foreground">
+              Please select your preferred payment method
+            </p>
+            <div className="flex flex-col gap-5 md:flex-row">
+              <FormField
+                control={form.control}
+                name="type"
+                render={({ field }) => (
+                  <FormItem className="space-y-3">
+                    <FormControl>
+                      <RadioGroup
+                        onValueChange={field.onChange}
+                        className="flex flex-col space-y-2"
+                      >
+                        {PAYMENT_METHODS.map((paymentMethod) => (
+                          <FormItem
+                            key={paymentMethod}
+                            className="flex items-center space-x-3 space-y-0"
+                          >
+                            <FormControl>
+                              <RadioGroupItem
+                                value={paymentMethod}
+                                checked={field.value === paymentMethod}
+                              />
+                            </FormControl>
+                            <FormLabel className="font-normal">
+                              {paymentMethod}
+                            </FormLabel>
+                          </FormItem>
+                        ))}
+                      </RadioGroup>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex gap-2">
+              <Button type="submit" disabled={isPending}>
+                {isPending ? (
+                  <Loader className="animate-spin w-4 h-4" />
+                ) : (
+                  <ArrowRight className="w-4 h-4" />
+                )}
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </>
   );
 };
