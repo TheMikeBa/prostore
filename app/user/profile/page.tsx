665817e6293c0afe254d0e9596138471c33e5ee3
TheMikeBa <21992394+TheMikeBa@users.noreply.github.com>
2025-06-20 18:04:44 -0400

    7. Update Profile Form

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,5 +1,22 @@
-const ProfilePage = () => {
-  return <div>Profile</div>;
+import { redirect } from "next/navigation";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
+import { Metadata } from "next";
+import ProfileForm from "./profile-form";
+
+export const metadata: Metadata = {
+  title: "Customer Profile",
 };
 
-export default ProfilePage;
+export default async function ProfilePage() {
+  const session = await auth();
+
+  return (
+    <SessionProvider session={session}>
+      <div className="max-w-md  mx-auto space-y-4">
+        <h2 className="h2-bold">Profile</h2>
+        <ProfileForm />
+      </div>
+    </SessionProvider>
+  );
+}
