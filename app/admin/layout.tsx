77ea25de464801545c61d0cad36dbfdf2247a508
TheMikeBa <21992394+TheMikeBa@users.noreply.github.com>
2025-06-23 19:01:38 -0400

    7. Admin Search Form

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,9 +1,9 @@
 import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
-import { Input } from "@/components/ui/input";
 import Menu from "@/components/shared/header/menu";
 import MainNav from "./main-nav";
+import AdminSearch from "@/components/admin/admin-search";
 
 export default async function AdminLayout({
   children,
@@ -26,11 +26,10 @@ export default async function AdminLayout({
             <MainNav className="mx-6" />
             <div className="ml-auto flex items-center space-x-4">
               <div>
-                <Input
-                  type="search"
-                  placeholder="Search..."
-                  className="md:w-[100px] lg:w-[300px]"
-                />
+                <div className="ml-auto flex items-center space-x-4">
+                  <AdminSearch />
+                  <Menu />
+                </div>
               </div>
               <Menu />
             </div>
