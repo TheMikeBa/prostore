5d3e428b894cbb19728db72c3f56772d40b4e196
TheMikeBa <21992394+TheMikeBa@users.noreply.github.com>
2025-06-22 21:03:53 -0400

    12. Is Featured & Banner

--- components/admin/product-form.tsx
+++ components/admin/product-form.tsx
@@ -22,7 +22,7 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
-// import { Checkbox } from "@/components/ui/checkbox";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { UploadButton } from "@/lib/uploadthing";
@@ -90,6 +90,8 @@ const ProductForm = ({
   };
 
   const images = form.watch("images");
+  const isFeatured = form.watch("isFeatured");
+  const banner = form.watch("banner");
 
   return (
     <Form {...form}>
@@ -296,7 +298,53 @@ const ProductForm = ({
             )}
           />
         </div>
-        <div className="upload-field">{/* Is Featured */}IS FEATURED</div>
+        <div className="upload-field">
+          {/* Is Featured */}
+          Featured Product
+          <Card>
+            <CardContent className="space-y-2 mt-2  ">
+              <FormField
+                control={form.control}
+                name="isFeatured"
+                render={({ field }) => (
+                  <FormItem className="flex flex-row space-x-2 items-center">
+                    <FormControl>
+                      <Checkbox
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                    <FormLabel>Is Featured?</FormLabel>
+                  </FormItem>
+                )}
+              />
+              {isFeatured && banner && (
+                <Image
+                  src={banner}
+                  alt="banner image"
+                  className=" w-full object-cover object-center rounded-sm"
+                  width={1920}
+                  height={680}
+                />
+              )}
+              {isFeatured && !banner && (
+                <UploadButton
+                  endpoint="imageUploader"
+                  onClientUploadComplete={(res: { url: string }[]) => {
+                    form.setValue("banner", res[0].url);
+                  }}
+                  onUploadError={(error: Error) => {
+                    // toast({
+                    //   variant: "destructive",
+                    //   description: `ERROR! ${error.message}`,
+                    // });
+                    toast.error(error.message);
+                  }}
+                />
+              )}
+            </CardContent>
+          </Card>
+        </div>
         <div>
           {/* Description */}
           <FormField
