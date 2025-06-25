a35f7985a2b769c8de0d5266513f37af90d1b2e4
TheMikeBa <21992394+TheMikeBa@users.noreply.github.com>
2025-06-24 20:18:46 -0400

    5. Search Page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,7 @@ import {
   getLatestProducts,
 } from "@/lib/actions/product.actions";
 import { ProductCarousel } from "@/components/shared/product/product-carousel";
+import ViewAllProductsButton from "@/components/view-all-products-button";
 
 const HomePage = async () => {
   const latestProducts = await getLatestProducts();
@@ -16,6 +17,7 @@ const HomePage = async () => {
       )}
 
       <ProductList title="Newest Arrivals" data={latestProducts} />
+      <ViewAllProductsButton />
     </div>
   );
 };
