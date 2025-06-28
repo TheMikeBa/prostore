a9a67d415844d84039410ee6dafe597099949115
TheMikeBa <21992394+TheMikeBa@users.noreply.github.com>
2025-06-27 22:50:49 -0400

    2. Icon Boxes Component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,7 @@ import {
 } from "@/lib/actions/product.actions";
 import { ProductCarousel } from "@/components/shared/product/product-carousel";
 import ViewAllProductsButton from "@/components/view-all-products-button";
+import IconBoxes from "@/components/icon-boxes";
 
 const HomePage = async () => {
   const latestProducts = await getLatestProducts();
@@ -18,6 +19,7 @@ const HomePage = async () => {
 
       <ProductList title="Newest Arrivals" data={latestProducts} />
       <ViewAllProductsButton />
+      <IconBoxes />
     </div>
   );
 };
